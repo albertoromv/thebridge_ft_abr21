4cd6b4faa9e67a2c7da65149f7a22d6777dff7fb
Alberto <albertoromv@hotmail.com>
2022-07-18 12:26:26 +0200

    update
    update

--- 5-Productivization/1-Flask/2-ML&Flask/app.py
+++ 5-Productivization/1-Flask/2-ML&Flask/app.py
@@ -5,15 +5,22 @@ import os
 
 os.chdir(r'C:\Users\Admin\Documents\GitHub\thebridge_ft_abr21\5-Productivization\1-Flask\2-ML&Flask')
 
-
-
 model = pickle.load(open('iri.pkl', 'rb'))
-
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET"])
-def home():
+def man():
     return render_template("home.html")
+
+@app.route("/predict", methods=["POST"])
+def home():
+    data1 = request.form['a']
+    data2 = request.form['b']
+    data3 = request.form['c']
+    data4 = request.form['d']
+    arr = np.array([[data1, data2, data3, data4]])
+    pred = model.predict(arr)
+    return render_template("after.html", data = pred)
 
 
 if __name__ == "__main__":
